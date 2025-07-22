5e51f80e4a4a2b8ec79b525411fd84d0556f1b15
zCooper-R <qucooper@yandex.ru>
2025-07-22 22:51:35 +0300

    initial commit

--- glpi_async/managers/tickets.py
+++ glpi_async/managers/tickets.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from datetime import datetime
 
-import aiohttp
-
 from .base import BaseManager
 
 
@@ -32,30 +30,6 @@ class TicketManager(BaseManager):
         if time_to_resolve:
             payload['input']["time_to_resolve"] = time_to_resolve.isoformat(timespec="minutes")
         return payload
-
-    async def upload_file(
-        self,
-        ticket_id: int,
-        file_bytes: bytes,
-        filename: str,
-        mime_type: str = "image/png"
-    ):
-        form = aiohttp.FormData()
-        form.add_field("uploadManifest", "1")
-        form.add_field("filename", file_bytes, filename=filename, content_type=mime_type)
-
-        document = await self.api.post("/Document", data=form)
-
-        await self.api.post("/Document_Item", json={
-            "input": {
-                "documents_id": document["id"],
-                "itemtype": "Ticket",
-                "items_id": ticket_id
-            }
-        })
-
-        return document
-
 
     async def list(self, params=None):
         return await self.api.get(f"{self.item_name}", params=params)
@@ -116,5 +90,4 @@ class TicketManager(BaseManager):
         if self.cache:
             await self.cache.set(cache_key, result, ttl=300)
         return result
-
 
