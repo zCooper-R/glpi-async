864c3c8dce6799a59499f2c7b04abf363e7ff6b2
zCooper-R <qucooper@yandex.ru>
2025-07-23 22:18:47 +0300

    main changed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from pprint import pprint
 
 from glpi_async.client import GLPIClient
@@ -9,15 +9,33 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-async def main():
+async def weekly_summary():
+    seven_days_ago = (datetime.now() - timedelta(days=7))
     async with GLPIClient(
             app_token=os.getenv("GLPI_APP_TOKEN"),
             user_token=os.getenv("GLPI_USER_TOKEN"),
             base_url=os.getenv("GLPI_API_URL")
     ) as glpi:
-        # glpiID = await glpi.users.get_my_id()
-        tickets = await glpi.categories.list_sorted_by_name()
-        print(tickets)
+        closed = await glpi.tickets.query().show('22').created_after(seven_days_ago).where(12, 6).get()
+        opened = await glpi.tickets.query().created_after(seven_days_ago).where(12, 2).get()
+        from collections import Counter
+        assigned_ids = [row.get("Ticket.users_id_recipient.User.name") for row in closed.get('data')]
+        most_common_username = Counter(assigned_ids).most_common(1)
+        # Формируем сообщение
+        text = (
+            f"📊 За неделю:\n"
+            f"✅ Закрыто: {closed.get('totalcount')} заявок\n"
+            f"🚨 Открыто: {opened.get('totalcount')} новых\n"
+            f"👤 Самый активный техник: {most_common_username[0][0]}"
+        )
+
+        return text
+
+
+async def main():
+    a = await weekly_summary()
+    print(a)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
