560379de30074a8d806eff50291b8b8efe262cb9
zCooper-R <qucooper@yandex.ru>
2025-07-23 14:16:10 +0300

    initial commit
    Took 9 minutes
    
    
    Took 1 minute

--- main.py
+++ main.py
@@ -16,20 +16,8 @@ async def main():
             base_url=os.getenv("GLPI_API_URL")
     ) as glpi:
         # glpiID = await glpi.users.get_my_id()
-        tickets = await glpi.tickets.list_open_tickets_with_assignees()
-        message_lines = ["🔔 Открытые заявки GLPI:\n"]
-        for ticket in tickets.get('data'):
-            # Форматируем вывод, например:
-            ticket_id = ticket.get("Ticket.id", "Без названия")  # имя заявки
-            ticket_name = ticket.get("Ticket.name", "Без названия")  # имя заявки
-            assignee_name = ticket.get("Ticket.users_id_recipient.User.name", "Не назначен")  # id ответственного
-            content = ticket.get("Ticket.content", "Не назначен")  # имя ответственного
-
-            message_lines.append(f"#{ticket_id}: {ticket_name}\nОтветственный: {assignee_name} (Описание: {content})\n")
-
-        message_text = "\n".join(message_lines)
-        print(message_text)
-
+        tickets = await glpi.categories.list()
+        print(tickets)
 
 if __name__ == '__main__':
     asyncio.run(main())
